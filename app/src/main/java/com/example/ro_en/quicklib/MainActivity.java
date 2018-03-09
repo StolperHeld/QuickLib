b22c52123350785181da90a6ee7c542183e27120
ions1995 <thomas1995>
2018-03-09 12:02:48 +0100

    generated MainAcivity & set oClickListener on fab

---
+++ app/src/main/java/com/example/ro_en/quicklib/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ro_en.quicklib;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton addBookShelfBtn = (FloatingActionButton) findViewById(R.id.addBookShelfBtn);
+        addBookShelfBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        addBookShelfBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                AlertDialog.Builder alertDialog;
+                alertDialog = new AlertDialog.Builder(MainActivity.this);
+                alertDialog.create();
+                alertDialog.setTitle("BookShelf");
+                alertDialog.setMessage("insert here the neme from your new BookShelf");
+                final EditText bookShelfname = new EditText(MainActivity.this);
+                alertDialog.setCancelable(false);
+                alertDialog.setPositiveButton("add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+            }
+
+        });
+
+
+
+    }
+
+}
