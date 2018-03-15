94bc25f77f78a7ca09b31c2221d0600b91136c5a
robertengel <akafallenhero@googlemail.com>
2018-03-15 16:17:53 +0100

    AsyncTask Implementation

--- app/src/main/java/com/example/ro_en/quicklib/HttpRequestBuilder.java
+++ app/src/main/java/com/example/ro_en/quicklib/HttpRequestBuilder.java
@@ -1,6 +1,8 @@
 package com.example.ro_en.quicklib;
 
 import android.content.SyncStatusObserver;
+import android.os.AsyncTask;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -10,29 +12,34 @@ import java.net.URL;
  * Created by thoma on 11.03.2018.
  */
 
-public class HttpRequestBuilder {
+public class HttpRequestBuilder extends AsyncTask<String, Void, String> {
 
     public String buildHttpRequest(String ISBN) throws Exception{
         String url = "https://openlibrary.org/api/books?bibkeys=ISBN:"+ISBN +"&jscmd=data&format=json";
         System.out.println(url);
         String json;
-        json = getUrlContent(url) ;
+        json = doInBackground(url) ;
         System.out.println(json);
         return url;
     }
 
-    public static String getUrlContent(String sUrl) throws Exception {
-        URL url = new URL(sUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setDoOutput(true);
-        connection.setConnectTimeout(5000);
-        connection.setReadTimeout(5000);
-        connection.connect();
-        BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    @Override
+    protected String doInBackground(String... sUrl){
         String content = "", line;
-        while ((line = rd.readLine()) != null) {
-            content += line + "\n";
+        try{
+            URL url = new URL(sUrl[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.connect();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while ((line = rd.readLine()) != null) {
+                content += line + "\n";
+            }
+        } catch(Exception e){
+            e.printStackTrace();
         }
         return content;
     }
