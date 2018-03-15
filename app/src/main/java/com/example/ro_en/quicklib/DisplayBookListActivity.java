8df8b0f58070a9297b03202591236e47b57a4129
ions1995 <thomas1995>
2018-03-15 19:05:30 +0100

    push with bugs

--- app/src/main/java/com/example/ro_en/quicklib/DisplayBookListActivity.java
+++ app/src/main/java/com/example/ro_en/quicklib/DisplayBookListActivity.java
@@ -1,13 +1,43 @@
 package com.example.ro_en.quicklib;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DisplayBookListActivity extends AppCompatActivity {
+
+    FloatingActionButton fabAddBook;
+    FloatingActionButton fabScannBook;
+    Boolean isFABOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_book_list);
+        FloatingActionButton fabMenu = (FloatingActionButton) findViewById(R.id.fab_menu);
+        fabAddBook = (FloatingActionButton) findViewById(R.id.fab_add_book);
+        fabScannBook = (FloatingActionButton) findViewById(R.id.fab_scann_book);
+        fabMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!isFABOpen){
+                    showFABMenu();
+                }else{
+                    closeFABMenu();
+                }
+            }
+        });
+    }
+    private void showFABMenu(){
+        isFABOpen=true;
+        fabAddBook.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+        fabScannBook.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+    }
+
+    private void closeFABMenu(){
+        isFABOpen=false;
+        fabAddBook.animate().translationY(0);
+        fabScannBook.animate().translationY(0);
     }
 }
