2d6e9e010de5999b60691381292c913caf86b678
ions1995 <thomas1995>
2018-03-15 18:46:21 +0100

    implemented code for testing scanning and request-building

---
+++ app/src/main/java/com/example/ro_en/quicklib/DisplayBookListActivity.java
@@ -0,0 +1,13 @@
+package com.example.ro_en.quicklib;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayBookListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_book_list);
+    }
+}
