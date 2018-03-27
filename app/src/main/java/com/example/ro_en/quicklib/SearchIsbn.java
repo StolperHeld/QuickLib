9c3d33b876ba065e19fd02e7dead6c558260a382
robertengel <akafallenhero@googlemail.com>
2018-03-27 13:51:17 +0200

    Added ISBN

--- app/src/main/java/com/example/ro_en/quicklib/SearchIsbn.java
+++ app/src/main/java/com/example/ro_en/quicklib/SearchIsbn.java
@@ -3,11 +3,12 @@ package com.example.ro_en.quicklib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SearchIsbn extends AppCompatActivity {
+public class SearchIsbn extends NavigationDrawerActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_isbn);
+        //setContentView(R.layout.activity_search_isbn);
+        getLayoutInflater().inflate(R.layout.activity_search_isbn, frameLayout);
     }
 }
