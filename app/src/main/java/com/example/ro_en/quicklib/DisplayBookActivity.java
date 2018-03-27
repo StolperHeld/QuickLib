e576b7afb2a937228fda4d7ccdd36470f0a3195b
ions1995 <thomas1995>
2018-03-27 15:15:30 +0200

    implemented ImageView to DisplayBookActivity

--- app/src/main/java/com/example/ro_en/quicklib/DisplayBookActivity.java
+++ app/src/main/java/com/example/ro_en/quicklib/DisplayBookActivity.java
@@ -1,10 +1,12 @@
 package com.example.ro_en.quicklib;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.ro_en.quicklib.firebase.FirebaseMethods;
@@ -15,9 +17,17 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class DisplayBookActivity extends NavigationDrawerActivity {
 
-    private TextView displayBookTitle, displayBookIsbn, displayBookAuthor;
+    private TextView displayBookTitle, displayBookIsbn, displayBookAuthor, displayBookPublisher,
+    displayBookPublisherPlace, displayBookPublisherDate, displayBookPages;
+    private ImageView displayBookImage;
+    URL imageUrl;
     private static final String TAG = "MainActivity-Log";
 
 
@@ -31,6 +41,11 @@ public class DisplayBookActivity extends NavigationDrawerActivity {
         displayBookTitle = (TextView) findViewById(R.id.diplayBookTitle);
         displayBookIsbn = (TextView) findViewById(R.id.displayBookIsbn);
         displayBookAuthor = (TextView) findViewById(R.id.diplayBookAuthor);
+        displayBookPublisher = (TextView) findViewById(R.id.displayBookPublisher);
+        displayBookPublisherPlace = (TextView) findViewById(R.id.diplayBookPublisherPlace);
+        displayBookPublisherDate = (TextView) findViewById(R.id.displayBookPublisherDate);
+        displayBookPages = (TextView) findViewById(R.id.displayBookPages);
+        displayBookImage = (ImageView) findViewById(R.id.displayBookImage);
 
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         final DocumentReference docRef = db.collection("books").document(bookId);
@@ -45,6 +60,15 @@ public class DisplayBookActivity extends NavigationDrawerActivity {
                         displayBookTitle.setText(book.getBookTitle());
                         displayBookIsbn.setText(book.getBookIsbn());
                         displayBookAuthor.setText(book.getBookAuthor());
+                        displayBookPublisher.setText(book.getBookPublisher());
+                        displayBookPublisherPlace.setText(book.getBookPublisherPlace());
+                        displayBookPublisherDate.setText(book.getBookPublisherDate());
+                        displayBookPages.setText(book.getBookPages() + "");
+                        loadImageFromURL(book.getBookImageUrl(),displayBookImage);
+
+                        //https://android--code.blogspot.de/2015/08/android-imageview-set-image-from-url.html
+                        //TODO: ImageView mit URL befüllen
+
                     } else {
                         Log.d(TAG, "No such document");
                     }
@@ -61,5 +85,28 @@ public class DisplayBookActivity extends NavigationDrawerActivity {
 
 
 
+    }
+    public boolean loadImageFromURL(String fileUrl,
+                                    ImageView iv){
+        try {
+
+            URL myFileUrl = new URL (fileUrl);
+            HttpURLConnection conn =
+                    (HttpURLConnection) myFileUrl.openConnection();
+            conn.setDoInput(true);
+            conn.connect();
+
+            InputStream is = conn.getInputStream();
+            iv.setImageBitmap(BitmapFactory.decodeStream(is));
+
+            return true;
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
     }
 }
